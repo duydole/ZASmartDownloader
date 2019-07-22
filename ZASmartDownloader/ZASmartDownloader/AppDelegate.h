292835d9b33c2cab76949e6cc77fe239c111c9c8
Duy Đỗ Lê <doleduy@icloud.com>
2019-07-22 11:20:02 +0700

    first commit

---
+++ ZASmartDownloader/ZASmartDownloader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZASmartDownloader
+//
+//  Created by Duy on 7/22/19.
+//  Copyright © 2019 zalo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZASmartDownloader/ZASmartDownloader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZASmartDownloader
+//
+//  Created by Duy on 7/22/19.
+//  Copyright © 2019 zalo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
